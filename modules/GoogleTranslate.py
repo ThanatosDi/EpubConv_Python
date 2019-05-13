ee5420f85a88ecbeac173af6b4c5f663411d3270
古丁丁 <s14113103@stu.edu.tw>
2019-05-14 01:45:29 +0800

    新增GoogleTranslate

---
+++ modules/GoogleTranslate.py
@@ -0,0 +1,29 @@
+import requests
+
+
+class GoogleTranslate:
+    def __init__(self, sl: str = 'auto', tl: str = 'zh-TW', value=None):
+        self.SourceLang = sl
+        self.TargetLang = tl
+        self.value = value
+        self.__response = self.__translate
+
+    @property
+    def __translate(self):
+        with requests.get(f'https://translate.googleapis.com/translate_a/single?client=gtx&sl={self.SourceLang}&tl={self.TargetLang}&dt=t&ie=utf-8&oe=utf-8&q={self.value}') as req:
+            if req.status_code != 200:
+                return None
+            req.encoding = 'utf-8'
+            return req.text
+
+    @property
+    def toList(self):
+        """Return data value from str to list
+        """
+        return self.__response.replace(']', '').replace('[', '').replace("\"", '').split(',')
+
+    @property
+    def response(self):
+        """Return response data
+        """
+        return self.__response
