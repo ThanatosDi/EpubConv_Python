d4b37e739d8edaac59cabcb63b41736f52f999bf
ThanatosDi <yykkold55tw@gmail.com>
2023-01-03 23:00:38 +0800

    搬移 config 位置

--- config/config.py
+++ config/config.py
@@ -1,5 +1,8 @@
 from dotenv import dotenv_values
 
+from app.Enums.ConverterEnum import ConverterEnum
+from app.Enums.EngineEnum import EngineEnum
+from app.Enums.FormatEnum import FormatEnum
 from app.modules.logger import Logger
 
 config = dotenv_values("config.ini")
@@ -9,32 +12,32 @@ logger = Logger(
     config.get('syslog_level', 'info').upper(),
 )
 
+
 def __engine() -> str:
-    ALLOW = ['opencc', 'fanhuaji', 'fanhuaji_async']
     engine = config.get('engine', 'opencc').lower()
-    if engine not in ALLOW:
+    if not EngineEnum.has_value(engine):
         logger.warning(f'轉換引擎不存在: {engine}，故使用預設引擎: opencc')
         config['engine'] = 'opencc'
         return 'opencc'
     return engine
 
+
 def __converter() -> str:
-    ALLOW = ['t2s', 's2t', 'tw2s', 's2tw', 'tw2sp', 's2twp']
     converter = config.get('converter', 's2t').lower()
-    if converter not in ALLOW:
+    if not ConverterEnum.converter.value.has_name(converter):
         logger.warning(f'轉換器不存在: {converter}，故使用預設轉換器: s2t')
         config['converter'] = 's2t'
         return 's2t'
     return converter
 
+
 def __format() -> str:
-    ALLOW = ['straight', 'horizontal']
-    format = config.get('format', 'horizontal').lower()
-    if format not in ALLOW:
-        logger.warning(f'格式不存在: {format}，故使用預設格式: horizontal(水平)')
+    _format = config.get('format', 'horizontal').lower()
+    if not FormatEnum.has_value(_format):
+        logger.warning(f'格式不存在: {_format}，故使用預設格式: horizontal(橫書)')
         config['format'] = 'horizontal'
         return 'horizontal'
-    return format
+    return _format
 
 
 ENGINE = __engine()
@@ -42,5 +45,3 @@ CONVERTER = __converter()
 FORMAT = __format()
 LOGLEVEL = config.get('loglevel', 'debug').upper()
 STDLEVEL = config.get('stdlevel', 'info').upper()
-
-
