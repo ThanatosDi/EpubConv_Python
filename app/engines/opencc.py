c1080cdd5ce2508e26e0ca87a499d361db8f99b1
ThanatosDi <yykkold55tw@gmail.com>
2023-01-03 22:59:03 +0800

    feat: 使用 Enum

--- app/engines/opencc.py
+++ app/engines/opencc.py
@@ -1,5 +1,6 @@
 import opencc
 from app.engines.engineABC import Engine
+from app.Enums.ConverterEnum import ConverterEnum
 
 
 class OpenCCEngine(Engine):
@@ -7,5 +8,8 @@ class OpenCCEngine(Engine):
         ...
 
     def convert(self, converter: str, text: str, **kwargs) -> str:
+        if not ConverterEnum.opencc.value.has_value(converter):
+            raise ValueError(
+                f'converter "{converter}" is not in OpenccConverter')
         converter: opencc.OpenCC = opencc.OpenCC(converter)
         return converter.convert(text)
