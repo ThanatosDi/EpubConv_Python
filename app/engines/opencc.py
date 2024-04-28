eeb41ce31d036a20e801915193f34acb5039492c
ThanatosDi <yykkold55tw@gmail.com>
2024-04-28 19:04:41 +0800

    feat: 實作 opencc 轉換功能

--- app/engines/opencc.py
+++ app/engines/opencc.py
@@ -1,5 +1,7 @@
 import opencc
-from app.engines.engineABC import Engine
+from loguru import logger
+
+from app.Engines.engineABC import Engine
 from app.Enums.ConverterEnum import ConverterEnum
 
 
@@ -7,9 +9,20 @@ class OpenCCEngine(Engine):
     def __init__(self,):
         ...
 
-    def convert(self, converter: str, text: str, **kwargs) -> str:
+    def convert(self, converter: str, chapters: list = None, **kwargs) -> list[dict[str, str]]:
         if not ConverterEnum.opencc.value.has_value(converter):
+            logger.error(f'{converter} 並非支援的 OpenCC 轉換器')
             raise ValueError(
                 f'converter "{converter}" is not in OpenccConverter')
         converter: opencc.OpenCC = opencc.OpenCC(converter)
-        return converter.convert(text)
+        for index, chapter in enumerate(chapters):
+            chapters[index]['content'] = converter.convert(chapter['content'])
+        return chapters
+
+    def filename_convert(self, converter: str, filename: str) -> str:
+        if not ConverterEnum.opencc.value.has_value(converter):
+            logger.error(f'{converter} 並非支援的 OpenCC 轉換器')
+            raise ValueError(
+                f'converter "{converter}" is not in OpenccConverter')
+        converter: opencc.OpenCC = opencc.OpenCC(converter)
+        return converter.convert(filename)
