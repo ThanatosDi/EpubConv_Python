ba475b46b0a5e82f4187ede8a4caf33e600806a1
ThanatosDi <yykkold55tw@gmail.com>
2024-04-28 19:08:58 +0800

    feat: 實作多項方法
    - 替換 log 紀錄方法
    - 壓縮方法
    - 增加檔案編碼判斷
    - epub 內容轉換方法
    - epub 檔案名稱轉換

--- app/modules/epubconv.py
+++ app/modules/epubconv.py
@@ -1,19 +1,21 @@
-import logging
 import os
 import pathlib
+import shutil
 import sys
 import zipfile
 
-from app.modules.convert import Convert
-from app.modules.opf import OPF
-from app.modules.renamer import ReNamer
-from app.modules.zip import ZIP
+import chardet
+from loguru import logger
 
-logger = logging.getLogger('EpubConv')
+from app.Modules.convert import Convert
+from app.Modules.opf import OPF
+from app.Modules.renamer import ReNamer
+from app.Modules.zip import ZIP
 
 
 class EPUBConv():
     def __init__(self, epub_abs_path: str) -> None:
+        logger.info(f'正在處理 epub: {pathlib.Path(repr(epub_abs_path))}')
         self.work_path = os.path.abspath(
             os.path.join(sys.argv[0], os.path.pardir))
         self.epub_abs_path = pathlib.Path(repr(epub_abs_path).strip("'"))
@@ -21,6 +23,10 @@ class EPUBConv():
     def epub_extract(self) -> None:
         """解壓縮 epub 檔案"""
         ZIP.extract(self.epub_abs_path)
+
+    def epub_compress(self) -> None:
+        """壓縮 epub 檔案"""
+        ZIP.compress(self.epub_abs_path)
 
     @property
     def epub_file(self):
@@ -41,28 +47,51 @@ class EPUBConv():
             opf_absolute_path (str): OPF 檔案的絕對路徑
         """
         opf_file = OPF(opf_absolute_path+'.new')
-        opf_file.language() # 語言標籤轉換
+        opf_file.language()  # 語言標籤轉換
 
-    def content_convert(self, content_absolute_path: str) -> None:
+    def file_encoding(self, file):
+        with open(file, 'rb') as file:
+            detect = chardet.detect(file.read())
+        logger.debug(detect)
+        return detect['encoding']
+
+    def content_convert(self, content_absolute_paths: list) -> None:
         """內容檔案轉換
 
         Args:
-            content_absolute_path (str): 內容檔案的絕對路徑
+            content_absolute_paths (list): 內容檔案的絕對路徑
         """
-        logger.info('轉換檔案: ' + os.path.basename(content_absolute_path))
-        with open(content_absolute_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-        converted_content = Convert.convert(content)
-        with open(content_absolute_path+'.new', 'w', encoding='utf-8') as file:
-            file.write(converted_content)
+        chapters = []
+        for content_absolute_path in content_absolute_paths:
+            encoding = self.file_encoding(content_absolute_path)
+            with open(content_absolute_path, 'r', encoding=encoding) as file:
+                content = file.read()
+            chapter = {
+                'path': content_absolute_path,
+                'content': content
+            }
+            chapters.append(chapter)
+        converted_chapters = Convert.convert(chapters)
+        for chapter in converted_chapters:
+            with open(chapter['path']+'.new', 'w', encoding='utf-8') as file:
+                file.write(chapter['content'])
 
-    def file_rename(self, file_absolute_path: str) -> None:
+    def file_rename(self, files: list) -> None:
         """檔案重新命名
 
         Args:
-            file_absolute_path (str): 檔案的絕對路徑
+            files (str): 檔案的絕對路徑清單
         """
-        ReNamer(file_absolute_path).rename()
+        for file in files:
+            ReNamer(file).rename()
+
+    def clean(self) -> None:
+        """ 清除解壓縮後的檔案 """
+        if os.path.isdir(f'{self.epub_abs_path}_files'):
+            logger.info(f'刪除暫存檔案: {self.epub_abs_path}_files')
+            shutil.rmtree(f'{self.epub_abs_path}_files')
+        else:
+            logger.error(f'路徑: {self.epub_abs_path}_files 不存在，刪除失敗。')
 
 
 class EpubFile():
