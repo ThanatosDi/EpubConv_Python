3a5770e975660d9b935844d26865db3984d3c962
ThanatosDi <yykkold55tw@gmail.com>
2024-08-26 17:26:25 +0800

    test: 繁化姬翻譯測試

--- tests/test_fanhuaji.py
+++ tests/test_fanhuaji.py
@@ -1,5 +1,9 @@
 import asyncio
-from app.engines.fanhuaji import FanhuajiEngine
+
+import aiohttp
+import pytest
+
+from app.Engines.fanhuaji import FanhuajiEngine
 from app.Enums.EngineEnum import EngineEnum
 
 
@@ -11,19 +15,36 @@ class TestFanhuaji():
 
     def test_fanhuaji_convert_s2t(self):
         params = {
-            'text': '使用 Python 撰写，epub 档案繁简横直互转',
+            'chapters': [
+                {'path': 'test_fanhuaji_convert_s2t.html',
+                 'content': '使用 Python 撰写，epub 档案繁简横直互转'}
+            ],
             'converter': 's2t'
         }
+        ans = [{'content': '使用 Python 撰寫，epub 檔案繁簡橫直互轉',
+                'path': 'test_fanhuaji_convert_s2t.html'}]
         text = self.fanhuaji.convert(**params)
-        print(text)
-        assert text == '使用 Python 撰寫，epub 檔案繁簡橫直互轉'
+        assert text == ans
 
-    def test_fanhuaji_convert_s2t_async(self):
+    @pytest.mark.asyncio
+    async def test_async_convert(self):
+        asyncio.set_event_loop_policy(
+            asyncio.WindowsSelectorEventLoopPolicy())
         params = {
-            'text': '使用 Python 撰写，epub 档案繁简横直互转',
+            'chapters': [
+                {'path': 'test_fanhuaji_convert_s2t_async.html',
+                 'content': '使用 Python 撰写，epub 档案繁简横直互转'},
+                {'path': 'test_fanhuaji_convert_s2t_async2.html',
+                 'content': '并且使用异步处理'},
+                {'path': 'test_fanhuaji_convert_s2t_async3.html',
+                 'content': '非同期処理を使用する'}
+            ],
             'converter': 's2t'
         }
-        text = asyncio.run(
-            self.fanhuaji.async_convert(**params)
-        )
-        assert text == '使用 Python 撰寫，epub 檔案繁簡橫直互轉'
+        ans = [
+            {'path': 'test_fanhuaji_convert_s2t_async.html',
+                'content': '使用 Python 撰寫，epub 檔案繁簡橫直互轉'},
+            {'path': 'test_fanhuaji_convert_s2t_async2.html', 'content': '並且使用異步處理'},
+            {'path': 'test_fanhuaji_convert_s2t_async3.html', 'content': '非同期処理を使用する'}]
+        result = await self.fanhuaji.async_convert(**params)
+        assert result == ans
